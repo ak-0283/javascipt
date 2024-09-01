4ece48af8434ed1af46728d5672824a652792075
Khush soni <sonikhush004@gmail.com>
2024-09-01 17:35:20 +0000

    variable opertions

---
+++ basic-code/variable.js
@@ -0,0 +1,7 @@
+ "use strict "
+ // using "use strict is like using the modern js where we dont use "var" key word //
+ let add = "khush";
+ let ad = "soni"
+
+ console.log(add + " " + ad);
+ // output = khush soni 
